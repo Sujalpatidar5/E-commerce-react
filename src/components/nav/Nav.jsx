41db3f9907c26a622094b25ef613dc3c324eec0b
Sujal Patidar <patidarsujal616@gmail.com>
2026-02-12 08:31:51 +0530

    Initial E-commerce setup

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaShopify } from "react-icons/fa6";
+import { IoSearchOutline } from "react-icons/io5";
+import { LuShoppingCart } from "react-icons/lu";
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <div className="nav">
+        <div className="top-nav">
+            <div className="logo">
+                <span>V-Shop</span>
+                <FaShopify />
+            </div>
+
+            <form className="search-box">
+                <input type="text" placeholder='Search Items..'/>
+                <button><IoSearchOutline /></button>
+            </form>
+
+            <div className="cart-box">
+                <LuShoppingCart />
+                <span>0</span>
+            </div>
+        </div>
+        <div className="bottom-nav">
+            <li>Home</li>
+            <li>Shop</li>
+            <li>Cart</li>
+            <li>Contact</li>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
